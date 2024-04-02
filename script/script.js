246f0fe3bad6bf60f5572a3ea6bf9f3d1aee0739
sadishaaaa <sthasadisha2019@gmail.com>
2024-04-02 08:11:22 +0545

    mobile menu functionality with transition

--- script/script.js
+++ script/script.js
@@ -8,5 +8,28 @@ crossButton.addEventListener("click", (e) => {
   announcementDiv.style.transform = "scaleY(0)";
   setTimeout(() => {
     announcementDiv.style.display = "none";
-  }, 250);
+  }, 200);
+});
+/**
+ * Mobile Menu Open Close Functionality
+ */
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const hamburgerClose = document.querySelector("#hamburger-close");
+const mobileNavDiv = document.querySelector(".mobile-menu");
+/**
+ * Opening Mobile Menu
+ */
+hamburgerMenu.addEventListener("click", () => {
+  mobileNavDiv.style.transform = "scaleY(1)";
+  hamburgerMenu.style.display = "none";
+  hamburgerClose.style.display = "block";
+});
+/**
+ * Closing Mobile Menu
+ */
+hamburgerClose.addEventListener("click", () => {
+  mobileNavDiv.style.transform = "scaleY(0)";
+  hamburgerMenu.style.display = "block";
+  hamburgerClose.style.display = "none";
+  menuWrapper.style.opacity = 0;
 });
